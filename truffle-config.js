680515163ad23fd5d1c4ffdbac7c47c2640cced6
Tianyu Yao <yaotianyu@berkeley.edu>
2022-05-11 22:08:10 -0400

    deploy to Huygens

--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,31 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const privateKey= require('./secrets.json').privateKey;
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8765,            // Standard Huygen port (default: none)
+      network_id: "*",       // Any network (default: none)
     },
-    develop: {
-      port: 8545
-    }
+    huygens: {
+      provider: () => new HDWalletProvider([privateKey], `https://huygens.computecoin.network/`),
+      network_id: 828,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
   compilers: {
     solc: {
-      version: "^0.8.0"
+      version: "^0.8.0", // A version or constraint - Ex. "^0.8.0"
     }
   }
-};
+}
